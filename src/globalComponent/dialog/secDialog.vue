c29be757f1fe5a92d70b20612f2ce89e51f76c93
owen <lwb@163.com>
2020-09-03 00:20:40 +0800

    尝试到inheritAttrs

--- src/globalComponent/dialog/secDialog.vue
+++ src/globalComponent/dialog/secDialog.vue
@@ -1,36 +1,59 @@
 <template>
-    <div>
-        <button @click="showDialog">打开Dialog</button>
-        <Dialog :visible.sync="visible" @confirm="confirm" @close="close">
-            <template #foot>
-                <button>封装</button>
-            </template>
-        </Dialog>
-    </div>
+  <Dialog v-bind="$attrs" v-on="$listeners">
+    <template #header>
+      <slot name="header"></slot>
+    </template>
+    <template #content>
+      <slot name="content">
+        <div>第二层content</div>
+        <div>
+          点击可以获取到第二层组件$attrs:
+          <button @click="logAttrs">click</button>
+        </div>
+
+        <div>
+          点击可以获取到第二层组件$listeners:
+          <button @click="logListeners">click</button>
+        </div>
+      </slot>
+    </template>
+    <template #foot>
+      <slot name="foot">
+        <button>第二层foot</button>
+      </slot>
+    </template>
+  </Dialog>
 </template>
 <script>
 import Dialog from "./index.vue";
 export default {
-    name: "secDialog",
-    data() {
-        return {
-            visible: false,
-        };
+  // 默认为true 此时外部传入的都可以在HTML上看到这个属性，设置为false时，只有没有被props的才会在本层的HTML标签属性显示。被props的没有必要显示
+
+  // 组件将不会把未被注册的props呈现为普通的HTML属性。但是在组件里我们可以通过其$attrs可以获取到没有使用的注册属性
+  // 通过设置 inheritAttrs 到 false，这些默认行为将会被去掉
+  //   inheritAttrs: false,
+  name: "secDialog",
+  data() {
+    return {};
+  },
+  props: ["isProps"],
+  methods: {
+    confirm() {
+      console.log("第二层确认");
     },
-    methods: {
-        showDialog() {
-            this.visible = true;
-        },
-        confirm() {
-            console.log("确认");
-        },
-        close() {
-            console.log("取消");
-        },
+    close() {
+      console.log("第二层取消");
     },
-    components: {
-        Dialog,
+    logAttrs() {
+      console.log(this.$attrs);
     },
+    logListeners() {
+      console.log(this.$listeners);
+    },
+  },
+  components: {
+    Dialog,
+  },
 };
 </script>
 <style lang="scss" scoped>
--- src/view/secDialog.vue
+++ src/view/secDialog.vue
@@ -1,36 +1,42 @@
 <template>
-    <div>
-        <button @click="showDialog">打开Dialog</button>
-        <Dialog :visible.sync="visible" @confirm="confirm" @close="close">
-            <template #foot>
-                <button>封装</button>
-            </template>
-        </Dialog>
-    </div>
+  <div>
+    <button @click="showDialog">打开Dialog</button>
+    <Dialog
+      :visible.sync="visible"
+      @confirm="confirm"
+      @close="close"
+      isProps="isProps"
+      isNoProps="isNotProps"
+    >
+      <template #foot>
+        <button>我是最外层的</button>
+      </template>
+    </Dialog>
+  </div>
 </template>
 <script>
-import Dialog from "@/globalComponent/dialog/index.vue";
+import Dialog from "@/globalComponent/dialog/secDialog.vue";
 export default {
-    name: "secDialog",
-    data() {
-        return {
-            visible: false,
-        };
+  name: "secDialog",
+  data() {
+    return {
+      visible: false,
+    };
+  },
+  methods: {
+    showDialog() {
+      this.visible = true;
     },
-    methods: {
-        showDialog() {
-            this.visible = true;
-        },
-        confirm() {
-            console.log("确认");
-        },
-        close() {
-            console.log("取消");
-        },
+    confirm() {
+      console.log("最外层确认");
     },
-    components: {
-        Dialog,
+    close() {
+      console.log("最外层取消");
     },
+  },
+  components: {
+    Dialog,
+  },
 };
 </script>
 <style lang="scss" scoped>
